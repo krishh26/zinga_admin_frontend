9fa356bc42be5477f54785d810a4f0089753cc1a
Kishansinh Parmar <kishansinhp26@gmail.com>
2025-02-20 17:37:34 +0530

    changes done

--- src/app/modules/payments/payments.module.ts
+++ src/app/modules/payments/payments.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaymentListComponent } from './payment-list/payment-list.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: 'payment-list', pathMatch: 'full' },
+   { path: 'payment-list', component: PaymentListComponent },
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PaymentListComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+       RouterModule.forChild(routes)
   ]
 })
 export class PaymentsModule { }
