450bc7610cef56216bf143852bee4cf0601c5287
krishh26 <kishansinhparmar999@gmail.com>
2025-03-06 00:07:27 +0530

    changes done

---
+++ src/app/modules/community/community-home/community-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-community-home',
+  templateUrl: './community-home.component.html',
+  styleUrls: ['./community-home.component.css']
+})
+export class CommunityHomeComponent {
+
+}
--- src/app/modules/community/community.module.ts
+++ src/app/modules/community/community.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommunityHomeComponent } from './community-home/community-home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { SharedModule } from 'src/app/utility/shared/shared.module';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'community-home', pathMatch: 'full' },
+  { path: 'community-home', component: CommunityHomeComponent },
+];
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CommunityHomeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+       RouterModule.forChild(routes),
+       SharedModule,
+       NgxPaginationModule
   ]
 })
 export class CommunityModule { }
