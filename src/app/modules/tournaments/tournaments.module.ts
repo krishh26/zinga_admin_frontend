e7413d286586f61ff2b134dbbbd99dd22be3cc10
krishh26 <kishansinhparmar999@gmail.com>
2025-03-25 11:59:50 +0530

    date add

--- src/app/modules/tournaments/tournaments.module.ts
+++ src/app/modules/tournaments/tournaments.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateTournamentsComponent } from './create-tournaments/create-tournaments.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { SharedModule } from 'src/app/utility/shared/shared.module';
 
 const routes: Routes = [
   { path: 'tournament', component: TournamentsComponent },
@@ -22,7 +23,8 @@ const routes: Routes = [
     FormsModule,
     ReactiveFormsModule,
     NgSelectModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ],
   exports: [
     TournamentsComponent,
